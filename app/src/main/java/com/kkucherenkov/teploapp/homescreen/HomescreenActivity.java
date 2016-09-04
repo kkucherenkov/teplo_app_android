588aae3386ad79e679038cda328d61e2db972bb7
Kirill Kucherenkov <kirill.kucherenkov@gmail.com>
2016-09-04 17:47:36 +0300

    add home screen fragment to home screen activity

--- app/src/main/java/com/kkucherenkov/teploapp/homescreen/HomescreenActivity.java
+++ app/src/main/java/com/kkucherenkov/teploapp/homescreen/HomescreenActivity.java
@@ -1,5 +1,6 @@
 package com.kkucherenkov.teploapp.homescreen;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,26 @@ import com.kkucherenkov.teploapp.R;
 
 public class HomescreenActivity extends AppCompatActivity {
 
+    private static final String TAG = HomescreenFragment.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(null);
+        showHomescreenFragment();
+    }
+
+    private void showHomescreenFragment() {
+        if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container_root, getFragment(), TAG)
+                    .commit();
+        }
+    }
+
+    public Fragment getFragment() {
+        return HomescreenFragment.newInstance("", "");
     }
 }
