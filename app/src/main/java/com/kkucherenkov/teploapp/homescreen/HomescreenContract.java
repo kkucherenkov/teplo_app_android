9aefd1bc09db9bbedd940e08a44e287af08d616c
Kirill Kucherenkov <kirill.kucherenkov@gmail.com>
2016-09-04 16:39:37 +0300

    add skeleton for home screen

---
+++ app/src/main/java/com/kkucherenkov/teploapp/homescreen/HomescreenContract.java
@@ -0,0 +1,15 @@
+package com.kkucherenkov.teploapp.homescreen;
+
+/**
+ * Created by Kirill Kucherenkov on 04/09/16.
+ */
+
+public interface HomescreenContract {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/kkucherenkov/teploapp/homescreen/HomescreenPresenterImpl.java
@@ -0,0 +1,8 @@
+package com.kkucherenkov.teploapp.homescreen;
+
+/**
+ * Created by Kirill Kucherenkov on 04/09/16.
+ */
+
+public class HomescreenPresenterImpl implements HomescreenContract.Presenter {
+}
